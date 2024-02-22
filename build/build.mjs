8046ce524f565c99a777342e908269180a99cb56
ian612 <34078802+ian612@users.noreply.github.com>
2024-02-21 20:22:26 -0500

    greatly simplified copying function

--- build/build.mjs
+++ build/build.mjs
@@ -9,32 +9,20 @@ const targetDirectory = path.join('./dist','');
 // A list of folders to ignore when we're copying files 1-to-1 (these are included in a different manner)
 const ignoreDirList = ['less', 'packs'];
 
-// A function to read all of the files in the source directory and store them in an array with their paths
-const fullFileList = [];
-export function readAllFiles(dir, depth) {
-    const files = fs.readdirSync(dir, { withFileTypes: true });
-    for (const file of files) {
-        if (!(ignoreDirList.includes(file.name) && depth === 0)) {
-            file.isDirectory() ? readAllFiles(path.join(dir, file.name), depth + 1) : fullFileList.push(file);
+// Copy files to a new directory
+fs.cpSync(sourceDirectory, targetDirectory, {
+    recursive: true,
+    filter: (file) => {
+        const fileBits = file.split(path.sep).slice(-2);
+        if (ignoreDirList.includes(fileBits[1]) && fileBits[0] === sourceDirectory) {
+            return false;
+        } else {
+            return true;
         }
-    }
-
-}
-
-readAllFiles(sourceDirectory, 0);
-console.log(fullFileList);
-
-// File destination.txt will be created or overwritten by default.
-/* fs.copyFile('source.txt', 'destination.txt', (err) => {
-  if (err) throw err;
-  console.log('source.txt was copied to destination.txt');
+    }},
+    (err) => {
+    if (err) throw err;
+    console.log('Copied!');
 });
 
-
-
-const sourceFiles = fs.readdirSync(sourceDirectory, { withFileTypes: true })
-    .map(dirent => dirent.name)
-
-console.log(sourceFiles)
-
-*/
+console.log('All Done')
