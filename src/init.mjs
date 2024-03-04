f22ef29ed42e88540b98e176cdf1bf268d8c1a41
ian612 <34078802+ian612@users.noreply.github.com>
2024-03-04 11:04:05 -0500

    finds item ID of item that was dropped onto

--- src/init.mjs
+++ src/init.mjs
@@ -1,3 +1,12 @@
 Hooks.once('ready', async () => {
     console.warn("Hey dawg what's going on? Not much dudeee.");
 });
+
+Hooks.on('dropActorSheetData', async (actor, sheet, data) => {
+    console.warn("Something just got DROPPED.");
+    console.log(actor, sheet, data, event);
+    const targetItemID = event.target.closest('li').getAttribute('data-item-id');
+    console.log(targetItemID);
+    console.log(event.target.closest('li'));
+    console.log(actor.items.get(targetItemID));
+});
